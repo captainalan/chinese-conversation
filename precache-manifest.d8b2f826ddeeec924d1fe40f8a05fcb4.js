d80b5abba722070f1b59e9ff51d91da7ac89f224
Alan Wong <captainalan@gmail.com>
2019-01-14 18:07:54 -0800

    Updates

---
+++ precache-manifest.d8b2f826ddeeec924d1fe40f8a05fcb4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b22ba80dd1c3c644df67",
+    "url": "/chinese-conversation/static/css/main.af8b1998.chunk.css"
+  },
+  {
+    "revision": "b22ba80dd1c3c644df67",
+    "url": "/chinese-conversation/static/js/main.b22ba80d.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/chinese-conversation/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "c7f55311f8749e2de4d9",
+    "url": "/chinese-conversation/static/js/runtime~main.c7f55311.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/chinese-conversation/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "c90385dfed507ed9a16ebff885ffc0b0",
+    "url": "/chinese-conversation/index.html"
+  }
+];
