7c2d73600eee5ba35f915c79ccb130bbfad35543
Alan Wong <captainalan@gmail.com>
2019-01-30 19:12:08 -0800

    Updates

---
+++ precache-manifest.dc1fcad158b6d1fa95c05736ed84575f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "2ac6faa6a4e80683a9da",
+    "url": "/chinese-conversation/static/css/main.1b8c647a.chunk.css"
+  },
+  {
+    "revision": "2ac6faa6a4e80683a9da",
+    "url": "/chinese-conversation/static/js/main.2ac6faa6.chunk.js"
+  },
+  {
+    "revision": "916b373cfef80c85a28b",
+    "url": "/chinese-conversation/static/js/1.916b373c.chunk.js"
+  },
+  {
+    "revision": "c7f55311f8749e2de4d9",
+    "url": "/chinese-conversation/static/js/runtime~main.c7f55311.js"
+  },
+  {
+    "revision": "87e9c67728a2274cbb5eb1f0aa8397e2",
+    "url": "/chinese-conversation/static/media/banner.87e9c677.png"
+  },
+  {
+    "revision": "efafd370108b4f5445293b1798947337",
+    "url": "/chinese-conversation/static/media/group_pic.efafd370.png"
+  },
+  {
+    "revision": "0f0485f3f79779d81dba33c35b8c1083",
+    "url": "/chinese-conversation/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/chinese-conversation/precache-manifest.15aa58607c9716137f242e7b69e0a286.js"
+  "/chinese-conversation/precache-manifest.dc1fcad158b6d1fa95c05736ed84575f.js"
 );
 
 workbox.clientsClaim();
