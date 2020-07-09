77f74581e6f3b249e9d6371ef193a366847fe6a2
Alan Wong <captainalan@gmail.com>
2020-07-09 10:59:50 -0700

    Updates

---
+++ precache-manifest.71762cf43e93e3df09ca2bb03f7e1e33.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82ba12d04c06b363af83b6cb2f0fd9b4",
+    "url": "/chinese-conversation/index.html"
+  },
+  {
+    "revision": "c8c2f77088d6e27689cd",
+    "url": "/chinese-conversation/static/css/main.2534917c.chunk.css"
+  },
+  {
+    "revision": "3f5c64d7fb2f6e2fe1d0",
+    "url": "/chinese-conversation/static/js/2.7198b8f8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/chinese-conversation/static/js/2.7198b8f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8c2f77088d6e27689cd",
+    "url": "/chinese-conversation/static/js/main.dbc34b46.chunk.js"
+  },
+  {
+    "revision": "45f1a179c9c66b71a097",
+    "url": "/chinese-conversation/static/js/runtime-main.4dbc4f70.js"
+  },
+  {
+    "revision": "87e9c67728a2274cbb5eb1f0aa8397e2",
+    "url": "/chinese-conversation/static/media/banner.87e9c677.png"
+  },
+  {
+    "revision": "efafd370108b4f5445293b1798947337",
+    "url": "/chinese-conversation/static/media/group_pic.efafd370.png"
+  }
+]);
